627379740131ccda3449fb685430001886217e9b
Karunanidhi16 <karunanidhisharma89@gmail.com>
2025-08-16 15:43:30 +0530

    utills code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';
 
 // import connectDB from './db';
-dotenv.config({path:'./.env'});
+dotenv.config({ path: './.env' });
 
 
 connectDB()
+    .then(() => {
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port ${process.env.PORT || 8000}`);
+        })
+    })
+app.on("error", (error) => {
+    console.log("errrror:", error);
+    throw error;
+})
+    .catch((error) => {
+console.log("MongoDB connection errrroorrr:",error);
+
+    })
 
 
 
